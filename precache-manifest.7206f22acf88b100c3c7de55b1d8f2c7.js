852b031866845bf076a9f4c63f755c35355d1c78
tweetmarken <glenn@tvetmarken.net>
2022-02-02 13:07:18 +0100

    Updates

---
+++ precache-manifest.7206f22acf88b100c3c7de55b1d8f2c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30a5cb18747d6c6b2d0e8a69bedd7b95",
+    "url": "/Rahoot/index.html"
+  },
+  {
+    "revision": "b4074af00286ca381f6e",
+    "url": "/Rahoot/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "2defc155ff70ada91cd5",
+    "url": "/Rahoot/static/css/main.aa7cdbea.chunk.css"
+  },
+  {
+    "revision": "b4074af00286ca381f6e",
+    "url": "/Rahoot/static/js/2.9ead89be.chunk.js"
+  },
+  {
+    "revision": "a2334ff9bc68c321bce67af5ebb4ec33",
+    "url": "/Rahoot/static/js/2.9ead89be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2defc155ff70ada91cd5",
+    "url": "/Rahoot/static/js/main.369cc33a.chunk.js"
+  },
+  {
+    "revision": "cca7adad6c5e28f659b8",
+    "url": "/Rahoot/static/js/runtime-main.a1c25819.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Rahoots/precache-manifest.31d0b86688c8fd6203dd83a969818d1a.js"
+  "/Rahoot/precache-manifest.7206f22acf88b100c3c7de55b1d8f2c7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Rahoots/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Rahoot/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
